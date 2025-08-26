6e7f953d44dc38df4dbe5e948c00eb180917abaa
salabibne <salabyaham@gmail.com>
2025-08-26 11:33:17 +0600

    Added Port

--- app.js
+++ app.js
@@ -67,5 +67,9 @@ app.use((err,req,res,next)=>{
   })
 })
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+})
 
 module.exports = app;
